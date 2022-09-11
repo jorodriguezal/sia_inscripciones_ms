9bc82615103e1159c851e51134ddedd9a154ad61
jorodriguezal <72422459+jorodriguezal@users.noreply.github.com>
2022-09-10 21:20:49 -0500

    First commit: initial models

---
+++ gestor_cursos/models.py
@@ -0,0 +1,47 @@
+from djongo import models
+
+# Create your models here.
+
+# MODELO DE CURSOS
+
+
+class Horario(models.Model):
+    # Día de la semana de la clase
+    dia = models.SmallIntegerField(max_length=1)
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+    salon = models.CharField(max_length=10)  # Salón de la clase
+    documento_profesor = models.CharField(
+        max_length=50)  # Profesor de la clase
+    # Tipo de clase (lab, teoría, taller, virtual, etc.)
+    tipo = models.CharField(max_length=20)
+
+    class Meta:
+        abstract = True
+
+
+class Curso(models.Model):
+    id_curso = models.AutoField(primary_key=True)
+    codigo_asignatura = models.CharField(max_length=30)
+    grupo = models.SmallIntegerField()
+    horarios = models.ArrayField(
+        model_container=Horario
+    )
+    cupos_disponibles = models.SmallIntegerField()
+    cupos_totales = models.SmallIntegerField()
+
+# MODELO DE ESTUDIANTES
+
+
+class Estudiante(models.Model):
+    documento_identidad = models.CharField(max_length=50, primary_key=True)
+    nombre_completo = models.CharField(max_length=100)
+    email_institucional = models.EmailField()
+
+# MODELO DE PROFESORES
+
+
+class Profesor(models.Model):
+    documento_identidad = models.CharField(max_length=50, primary_key=True)
+    nombre_completo = models.CharField(max_length=100)
+    email_institucional = models.EmailField()
