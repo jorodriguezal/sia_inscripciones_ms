add8346e1695ea8ad11f859fc819372b05f39b2d
jorodriguezal <72422459+jorodriguezal@users.noreply.github.com>
2022-10-01 16:36:44 -0500

    Se elimina todo lo referente a la bd de asignaturas

--- gestor_cursos/views.py
+++ gestor_cursos/views.py
@@ -6,7 +6,6 @@ from rest_framework import status
 
 from gestor_cursos.models import Curso, CursoInscrito, Profesor
 from gestor_cursos.serializers import CursoSerializer, CursoInscritoSerializer, ProfesorSerializer
-from asignaturas.models import Asignatura, Prerequisito
 
 # Create your views here.
 
@@ -21,24 +20,17 @@ def cursoApi(request, id=0):
 
     elif request.method == 'POST':
         curso_data = JSONParser().parse(request)
-
-        # Valida que la asignatura exista
-        try:
-            asignatura = Asignatura.objects.get(
-                codigo_asignatura=curso_data['codigo_asignatura'])
-        except Asignatura.DoesNotExist:
-            return JsonResponse({'message': 'La asignatura no existe'}, status=status.HTTP_404_NOT_FOUND)
         # valida que el curso no exista
         cursos_repetidos = Curso.objects.filter(
             id_curso=curso_data['id_curso'])
         if cursos_repetidos.count() > 0:
-            return JsonResponse("El curso ya existe", safe=False)
+            return JsonResponse("El curso ya existe", safe=False, status=status.HTTP_400_BAD_REQUEST)
         curso_serializer = CursoSerializer(data=curso_data)
 
         if curso_serializer.is_valid():
             curso_serializer.save()
             return JsonResponse("Agregado Correctamente", safe=False)
-        return JsonResponse("Fallo al agregar", safe=False)
+        return JsonResponse("Fallo al agregar", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'PUT':
         curso_data = JSONParser().parse(request)
@@ -48,7 +40,7 @@ def cursoApi(request, id=0):
         if curso_serializer.is_valid():
             curso_serializer.save()
             return JsonResponse("Actualizado Correctamente", safe=False)
-        return JsonResponse("Fallo al actualizar", safe=False)
+        return JsonResponse("Fallo al actualizar", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         curso = Curso.objects.get(id_curso=id)
@@ -67,23 +59,15 @@ def cursoInscritoApi(request, id=0):
 
     elif request.method == 'POST':
         curso_inscrito_data = JSONParser().parse(request)
-        asignaturas_cursadas = curso_inscrito_data['asignaturas_cursadas']
 
         cursos_inscritos = CursoInscrito.objects.filter(
             documento_estudiante=curso_inscrito_data['documento_estudiante'])
-
-        # valida que no haya cursado la asignatura antes
-        for asignatura in asignaturas_cursadas:
-            cursos_repetidos = Curso.objects.filter(
-                codigo_asignatura=asignatura, id_curso=curso_inscrito_data['id_curso'])
-            if cursos_repetidos.count() > 0:
-                return JsonResponse("El estudiante ya cursó la asignatura", safe=False)
 
         # valida que el curso no esté inscrito por el mismo estudiante
         cursos_inscritos_repetidos = CursoInscrito.objects.filter(
             id_curso=curso_inscrito_data['id_curso'], documento_estudiante=curso_inscrito_data['documento_estudiante'])
         if cursos_inscritos_repetidos.count() > 0:
-            return JsonResponse("El curso ya está inscrito por el estudiante", safe=False)
+            return JsonResponse("El curso ya está inscrito por el estudiante", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
         # Valida que el curso sea de una asignatura que el estudiante no haya ya inscrito
         cursos = Curso.objects.filter(
@@ -92,9 +76,10 @@ def cursoInscritoApi(request, id=0):
         asignaturas_inscritas = [curso.codigo_asignatura for curso in cursos]
         curso = Curso.objects.get(id_curso=curso_inscrito_data['id_curso'])
         if curso.codigo_asignatura in asignaturas_inscritas:
-            return JsonResponse("El estudiante ya está inscrito en una asignatura con el mismo código", safe=False)
+            return JsonResponse("El estudiante ya está inscrito en una asignatura con el mismo código", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
         # valida que cumpla el prerequisito o corequisito
+        '''
         prerequisitos = Prerequisito.objects.filter(
             codigo_asignatura=curso.codigo_asignatura)
         for prerequisito in prerequisitos:
@@ -105,7 +90,7 @@ def cursoInscritoApi(request, id=0):
                     else:
                         continue
                 return JsonResponse("El estudiante no cumple el prerequisito", safe=False)
-
+        '''
         # valida que el horario no se solape
         solapado = False
         for curso_inscrito in cursos_inscritos:
@@ -131,11 +116,11 @@ def cursoInscritoApi(request, id=0):
                             solapado = True
                             break
         if solapado:
-            return JsonResponse("El horario se solapa con otro curso", safe=False)
+            return JsonResponse("El horario se solapa con otro curso", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
         # Valida que el curso tenga cupos disponibles
         if curso.cupos_disponibles <= 0:
-            return JsonResponse("El curso no tiene cupos disponibles", safe=False)
+            return JsonResponse("El curso no tiene cupos disponibles", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
         curso_inscrito_serializer = CursoInscritoSerializer(
             data=curso_inscrito_data)
@@ -146,7 +131,7 @@ def cursoInscritoApi(request, id=0):
             curso.cupos_disponibles -= 1
             curso.save()
             return JsonResponse("Agregado Correctamente", safe=False)
-        return JsonResponse("Fallo al agregar", safe=False)
+        return JsonResponse("Fallo al agregar", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         curso_inscrito = CursoInscrito.objects.get(id_curso_inscrito=id)
@@ -168,13 +153,13 @@ def profesorApi(request, id=0):
         profesor_repetido = Profesor.objects.filter(
             documento_identidad=profesor_data['documento_identidad'])
         if profesor_repetido.count() > 0:
-            return JsonResponse("El profesor ya existe", safe=False)
+            return JsonResponse("El profesor ya existe", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
         profesor_serializer = ProfesorSerializer(data=profesor_data)
         if profesor_serializer.is_valid():
             profesor_serializer.save()
             return JsonResponse("Agregado Correctamente", safe=False)
-        return JsonResponse("Fallo al agregar", safe=False)
+        return JsonResponse("Fallo al agregar", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'PUT':
         profesor_data = JSONParser().parse(request)
